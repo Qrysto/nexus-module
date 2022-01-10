c781eb9cf16494ddba0581a682f9c1d44312c7ae
Krysto <quy.p.hoang@gmail.com>
2022-01-10 11:26:05 +0700

    added redux reducer & actions

--- src/redux.js
+++ src/redux.js
@@ -2,6 +2,10 @@ const {
   utilities: { updateState, updateStorage },
 } = NEXUS;
 
+export const INITIALIZE = '@@NWM/INITIALIZE';
+export const UPDATE_CORE_INFO = '@@NWM/UPDATE_CORE_INFO';
+export const UPDATE_THEME = '@@NWM/UPDATE_THEME';
+
 export function stateMiddleware(getPersistedState) {
   return (store) => (next) => (action) => {
     const oldData = getPersistedState(store.getState());
@@ -25,3 +29,64 @@ export function storageMiddleware(getStoredData) {
     return result;
   };
 }
+
+export const initialize = (data) => ({
+  type: INITIALIZE,
+  payload: data,
+});
+
+export const updateCoreInfo = (coreInfo) => ({
+  type: UPDATE_CORE_INFO,
+  payload: coreInfo,
+});
+
+export const updateTheme = (theme) => ({
+  type: UPDATE_THEME,
+  payload: theme,
+});
+
+const initialState = {
+  initialized: false,
+  coreInfo: null,
+  theme: null,
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INITIALIZE:
+      return {
+        ...state,
+        initialized: true,
+        coreInfo: action.payload.coreInfo,
+        theme: action.payload.theme,
+      };
+    case UPDATE_CORE_INFO:
+      return {
+        ...state,
+        coreInfo: action.payload.coreInfo,
+      };
+    case UPDATE_THEME:
+      return {
+        ...state,
+        initialized: true,
+        theme: action.payload.theme,
+      };
+    default:
+      return state;
+  }
+}
+
+export const initialize = (data) => ({
+  type: TYPE.INITIALIZE,
+  payload: data,
+});
+
+export const updateCoreInfo = (coreInfo) => ({
+  type: TYPE.UPDATE_CORE_INFO,
+  payload: coreInfo,
+});
+
+export const updateTheme = (theme) => ({
+  type: TYPE.UPDATE_THEME,
+  payload: theme,
+});
