5f9d7a7d46d7eac50039a1ab6b111ebdf47c145a
Krysto <quy.p.hoang@gmail.com>
2022-01-24 12:12:23 +0700

    fixed createCache

--- src/ModuleWrapper.js
+++ src/ModuleWrapper.js
@@ -1,7 +1,7 @@
 const nexus = global.NEXUS;
 const React = nexus?.libraries.React;
 const CacheProvider = nexus?.libraries.emotion.react.CacheProvider;
-const createCache = nexus?.libraries.emotion.cache.default;
+const createCache = nexus?.libraries.emotion.cache;
 const ThemeController = nexus?.components.ThemeController;
 const GlobalStyles = nexus?.components.GlobalStyles;
 
