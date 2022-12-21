3b31276f37061c0433d1d97508b5de821e4f478b
Krysto <quy.p.hoang@gmail.com>
2022-12-21 14:25:17 +0700

    renamed files for more consistent naming

--- src/webpackAliases.js
+++ src/webpackAliases.js
@@ -1,7 +1,7 @@
 export default {
   react$: 'nexus-module/lib/react.js',
-  'react/jsx-runtime$': 'nexus-module/lib/jsx-runtime.js',
-  'react/jsx-dev-runtime$': 'nexus-module/lib/jsx-dev-runtime.js',
+  'react/jsx-runtime$': 'nexus-module/lib/react-jsx-runtime.js',
+  'react/jsx-dev-runtime$': 'nexus-module/lib/react-jsx-dev-runtime.js',
   'react-dom$': 'nexus-module/lib/react-dom.js',
   'react-dom/client$': 'nexus-module/lib/react-dom-client.js',
   'react-dom/server$': 'nexus-module/lib/react-dom-server.js',
