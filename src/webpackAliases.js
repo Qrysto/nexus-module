96aead3a844b49c0bafdb05fe73857353506112d
Krysto <quy.p.hoang@gmail.com>
2025-06-05 09:26:00 +0700

    moved librarie proxies into separate folders

--- src/webpackAliases.js
+++ src/webpackAliases.js
@@ -1,10 +1,10 @@
 export default {
-  react$: "nexus-module/lib/react.js",
-  "react/jsx-runtime$": "nexus-module/lib/react-jsx-runtime.js",
-  "react/jsx-dev-runtime$": "nexus-module/lib/react-jsx-dev-runtime.js",
-  "react-dom$": "nexus-module/lib/react-dom.js",
-  "react-dom/client$": "nexus-module/lib/react-dom-client.js",
-  "@emotion/react$": "nexus-module/lib/emotionReact.js",
-  "@emotion/cache$": "nexus-module/lib/emotionCache.js",
-  "@emotion/styled$": "nexus-module/lib/emotionStyled.js",
+  react$: "nexus-module/lib/react/index",
+  "react/jsx-runtime$": "nexus-module/lib/react/jsx-runtime",
+  "react/jsx-dev-runtime$": "nexus-module/lib/react/jsx-dev-runtime",
+  "react-dom$": "nexus-module/lib/react-dom/index",
+  "react-dom/client$": "nexus-module/lib/react-dom/client",
+  "@emotion/react$": "nexus-module/lib/@emotion/react",
+  "@emotion/cache$": "nexus-module/lib/@emotion/cache",
+  "@emotion/styled$": "nexus-module/lib/@emotion/styled",
 };
